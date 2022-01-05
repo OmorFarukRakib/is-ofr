9a5f0ad1c28ccc9de55297b3f9b1801f850d247d
OmorFarukRakib <rakib1001@gmail.com>
2022-01-05 23:15:19 +0600

    update to 1.0.1

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'is-ofr'
 import 'is-ofr/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text="ARE YOU OFR" />
 }
 
 export default App
